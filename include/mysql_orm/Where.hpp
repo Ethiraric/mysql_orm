d96290dd5fde0bf20c897e4c2f6e5fd3dc4b9471
Ethiraric <floriansabourin@live.fr>
2018-06-18 09:56:35 +0200

    Move Where's DSL to separate file.

--- include/mysql_orm/Where.hpp
+++ include/mysql_orm/Where.hpp
@@ -6,66 +6,10 @@
 
 #include <mysql_orm/Limit.hpp>
 #include <mysql_orm/Statement.hpp>
+#include <mysql_orm/WhereConditionDSL.hpp>
 
 namespace mysql_orm
 {
-template <typename T>
-struct OperandWrapper
-{
-  template <typename Table>
-  void appendToQuery(std::ostream& out, Table const& t) const
-  {
-    (void)(t);
-    out << value;
-  }
-
-  T value;
-};
-
-template <typename Lhs, typename Rhs>
-struct EqualsClosure
-{
-  using compare_tag = int;
-
-  template <typename Table>
-  void appendToQuery(std::ostream& out, Table const& t) const
-  {
-    this->lhs.appendToQuery(out, t);
-    out << '=';
-    this->rhs.appendToQuery(out, t);
-  }
-
-  Lhs lhs;
-  Rhs rhs;
-};
-
-template <typename Lhs, typename Rhs>
-EqualsClosure(Lhs, Rhs)->EqualsClosure<Lhs, Rhs>;
-
-template <auto attr>
-struct c
-{
-  static inline constexpr auto attribute = attr;
-
-  template <typename Table>
-  void appendToQuery(std::ostream& out, Table const& t) const
-  {
-    out << '`' << t.template getColumn<attr>().getName() << '`';
-  }
-
-  template <typename T>
-  auto operator==(T const& rhs)
-  {
-    return EqualsClosure{*this, OperandWrapper<T>{rhs}};
-  }
-
-  template <auto other_attr>
-  auto operator==(c<other_attr> const& rhs)
-  {
-    return EqualsClosure{*this, rhs};
-  }
-};
-
 template <typename Condition>
 struct Where
 {
---
+++ include/mysql_orm/WhereConditionDSL.hpp
@@ -0,0 +1,66 @@
+#ifndef MYSQL_ORM_WHERECONDITIONDSL_HPP_
+#define MYSQL_ORM_WHERECONDITIONDSL_HPP_
+
+#include <ostream>
+
+namespace mysql_orm
+{
+template <typename T>
+struct OperandWrapper
+{
+  template <typename Table>
+  void appendToQuery(std::ostream& out, Table const& t) const
+  {
+    (void)(t);
+    out << value;
+  }
+
+  T value;
+};
+
+template <typename Lhs, typename Rhs>
+struct EqualsClosure
+{
+  using compare_tag = int;
+
+  template <typename Table>
+  void appendToQuery(std::ostream& out, Table const& t) const
+  {
+    this->lhs.appendToQuery(out, t);
+    out << '=';
+    this->rhs.appendToQuery(out, t);
+  }
+
+  Lhs lhs;
+  Rhs rhs;
+};
+
+template <typename Lhs, typename Rhs>
+EqualsClosure(Lhs, Rhs)->EqualsClosure<Lhs, Rhs>;
+
+template <auto attr>
+struct c
+{
+  static inline constexpr auto attribute = attr;
+
+  template <typename Table>
+  void appendToQuery(std::ostream& out, Table const& t) const
+  {
+    out << '`' << t.template getColumn<attr>().getName() << '`';
+  }
+
+  template <typename T>
+  auto operator==(T const& rhs)
+  {
+    return EqualsClosure{*this, OperandWrapper<T>{rhs}};
+  }
+
+  template <auto other_attr>
+  auto operator==(c<other_attr> const& rhs)
+  {
+    return EqualsClosure{*this, rhs};
+  }
+};
+}
+
+#endif /* !MYSQL_ORM_WHERECONDITIONDSL_HPP_ */
